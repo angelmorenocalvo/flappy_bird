8f4c3980280b47b563c2337dae4a265c04906c2e
angmore <angel.moreno.calvo@alumnos.uva.es>
2021-02-13 17:36:49 +0100

    estructura del juego

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var bird;
+var pipes = [];
+
+function setup(){
+    createCanvas(400, 600);
+    bird = new Bird();
+    pipes.push(new Pipe());
+}
+
+function draw(){
+    background(0);
+
+    for(var i = pipes.length-1; i>= 0; i--){
+        pipes[i].show();
+        pipes[i].update();
+
+        if(pipes[i].hits(bird)){
+            console.log("HIT");
+        }
+
+        if(pipes[i].offscreen()){
+            pipes.splice(i, 1); // elimina el tubo
+        }
+    }
+    
+    bird.update();
+    bird.show();
+
+    if(frameCount % 100 == 0){
+        pipes.push(new Pipe());
+    }
+
+    
+}
+/*
+for(var i = 0; i< pipes.length; i++){
+    pipes[i].show();
+    pipes[i].update();
+    if(pipes[i].offscreen()){
+        pipes.splice(i, 1); // elimina el tubo
+    }
+}
+}
+*/
+
+function keyPressed(){
+    if(key == ' '){
+        bird.up();
+    }
+}
